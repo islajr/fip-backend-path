51acd90c11d9259a4d4a3342ce9d658b4afa88f6
Isla <islarcherjr@icloud.com>
2025-05-28 21:56:05 +0100

    bare project files.

---
+++ 3-6-project-learning/workout-tracker-demo/src/main/java/org/project/workouttrackerdemo/WorkoutTrackerDemoApplication.java
@@ -0,0 +1,13 @@
+package org.project.workouttrackerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutTrackerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutTrackerDemoApplication.class, args);
+    }
+
+}
---
+++ 3-6-project-learning/workout-tracker-demo/src/test/java/org/project/workouttrackerdemo/WorkoutTrackerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.workouttrackerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
