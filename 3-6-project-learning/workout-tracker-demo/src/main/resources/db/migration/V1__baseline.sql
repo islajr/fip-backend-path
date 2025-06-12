84e6012d8fd84fc038aeccb1762557f73bb39c8b
Isla <islarcherjr@icloud.com>
2025-06-12 13:03:23 +0100

    update: added baseline migration.

---
+++ 3-6-project-learning/workout-tracker-demo/src/main/resources/db/migration/V1__baseline.sql
@@ -0,0 +1,5 @@
+-- V1__baseline.sql
+-- A baseline marker. No changes needed if the DB is already up-to-date.
+-- This file may either be empty or contain comments.
+
+-- CREATE TABLE statements can also be included if the schema is to be recreated in future deployments.
